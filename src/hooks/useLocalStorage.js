ca3aa746bae0807ac984517acacea70f53242cc7
Ahmet Buğra Yiğiter <54077855+yigiterdev@users.noreply.github.com>
2021-03-24 22:18:51 +0300

    first commit

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+const PREFIX = 'codepen-clone-';
+export default function useLocalStorage(key, initalValue) {
+  const prefixedKey = PREFIX + key;
+  const [value, setValue] = useState(() => {
+    const jsonValue = localStorage.getItem(prefixedKey);
+    if (jsonValue != null) return JSON.parse(jsonValue);
+
+    if (typeof initalValue === 'function') {
+      return initalValue();
+    } else {
+      return initalValue;
+    }
+  });
+  useEffect(() => {
+    localStorage.setItem(prefixedKey, JSON.stringify(value));
+  }, [prefixedKey, value]);
+  return [value, setValue];
+}
